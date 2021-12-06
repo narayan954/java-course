94c0924991c4b7b4997ae2bd3a82cc3b578f26f1
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-06 22:48:44 +0530

    Update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -1,145 +1,103 @@
-// Code for Beginning Java's Cake & Cupcake Shop Tutorial
-
-import java.util.Scanner;  // Needed for the Scanner class to read input
+import java.util.Scanner; 
 
 public class custom_order {
 
-    // STEP 1 PRINTING HELLO WORLD TO CONSOLE
+    static double totalCost; // Total Cost
+    static String addOnList; // List of addons 
+
+    static void addItem(String item, int cost) {
+            totalCost+=cost;
+            addOnList+=item;
+        }
+
     public static void main(String[] args) {
         
-    //System.out.println("Hello World!"); // print Hello World to console
-        
-    // TEST CODE
-    
-    // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
     	Scanner keyboard = new Scanner (System.in); 
-	
-	String firstName; // User's first name
-	String itemOrder; // Item ordered
-	String frostingType; // Frosting ordered
-	String fillingType; // Filling ordered
-	String toppings; // Toppings ordered
-	String input;  // User input
-	
-	double cost = 15.00; // Cost of cake and cupcakes
-	final double TAX_RATE = .08;  // Sales tax rate
-	double tax; // Amount of tax
-	// Introduce shop and prompt user to input first name
+        
+        String firstName; // User's first name
+        String itemOrder; // Item ordered
+        String frostingType; // Frosting ordered
+        String fillingType; // Filling ordered
+        String toppings; // Toppings ordered
+        String input;  // User input
 
-	System.out.println("Welcome to Java's Cake & Cupcake Shop!");
-	System.out.println("We make custom cakes with our secret cake batter!");
+        double cost = 15.00; // Cost of cake and cupcakes
+        final double TAX_RATE = .08;  // Sales tax rate
+        double tax; // Amount of tax
 
-	
-	  
+        
+        // Introduce shop 
+        System.out.println("Welcome to Java's Cake & Cupcake Shop!");
+        System.out.println("We make custom cakes with our secret cake batter!");
+        
+        // Prompt user to input first name
+        System.out.print("What is your first name? ");
+        firstName = keyboard.nextLine();
 
-	 
-    // Introduce shop and prompt user to input first name
-      
-
-	  
-	  
-    // TEST CODE
+        // Greet with Menu
+        System.out.print(firstName + ", please see our MENU below: ");
+        System.out.print("\n"); 
     
-    // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
-    System.out.print("What is your first name? ");
-firstName = keyboard.nextLine();
+        // DISPLAY MENU
+        System.out.println("_______________________________________________");      
+        System.out.println("        MENU         QUANTITY    BASE COST  ");
+        System.out.println("_______________________________________________"); 
+        System.out.println("        Cake             1            $15     ");
+        System.out.println("   Set of Cupcakes       6            $15     ");
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+        System.out.println("Frostings* (vanilla, chocolate, strawberry, coco)");
+        System.out.println("Fillings* (mocha, mint, lemon, caramel, vanilla)");
+        System.out.println("Toppings* (sprinkles, cinnamon, cocoa, nuts)");
+        System.out.println("*Frostings , Fillings and Toppings cost $2 each");
+        System.out.println("_______________________________________________");
 
-System.out.print(firstName + ", please see our MENU below: ");
-System.out.print("\n"); // skips a line	  
+        // PROMPT USER TO ORDER
+        System.out.println("Do you want CUPCAKES or a CAKE?");
+        itemOrder = keyboard.nextLine();
 
-   
-	  
-      
-    // TEST CODE     
-    
-    // STEP 4 DISPLAY MENU
-   	System.out.println("_______________________________________________");      
-	System.out.println("        MENU         QUANTITY    BASE COST  ");
-	System.out.println("_______________________________________________"); 
-	System.out.println("        Cake             1            $15     ");
-	System.out.println("   Set of Cupcakes       6            $15     ");
-	System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
-	System.out.println("Frostings (vanilla, chocolate, strawberry, coco)");
-	System.out.println("Fillings (mocha, mint, lemon, caramel, vanilla)");
-	System.out.println("Toppings (sprinkles, cinnamon, cocoa, nuts)");
-	System.out.println("_______________________________________________");
+        // PROMPT USER TO CHOOSE FROSTING
+        System.out.println("What type of FROSTING do you want? ");
+        System.out.println("Vanilla, Chocolate, Strawberry or Coco");
+        frostingType = keyboard.nextLine();
 
+        if (frostingType != "no"){
+          addItem(frostingType,2);
+          addOnList+=", ";
+        }
+      
+        // PROMPT USER TO CHOOSE FILLING
+        System.out.println("What type of FILLING do you want? ");
+        System.out.println("Mocha, Mint, Lemon, Caramel or Raspberry");
+        fillingType = keyboard.nextLine();
 
-	
+        if (fillingType != "no"){
+          addItem(fillingType,2);
+          addOnList+=", ";
+        }
       
+        // PROMPT USER TO CHOOSE TOPPINGS
+        System.out.println("What type of TOPPINGS do you want? ");
+        System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
+        toppings = keyboard.nextLine();
+        if (toppings != "no"){
+          addItem(toppings,2);
+          addOnList+=", ";
+        }
+      
+        // DISPLAY ORDER CONFIRMATION
+        System.out.println();
+        System.out.println(firstName + " , your order is as follows: ");
+        System.out.println("_________________________________________");
+        System.out.println("Item Ordered: " + itemOrder);
+        System.out.println("Frosting: " + frostingType);
+        System.out.println("Filling: " + fillingType);
+        System.out.println("Toppings: " + toppings);
+        System.out.println("_________________________________________");
 
-    // TEST CODE     
-    
-    // STEP 5 PROMPT USER TO ORDER
-	System.out.println("Do you want CUPCAKES or a CAKE?");
-	itemOrder = keyboard.nextLine();
-
-  
-	  
-	  
-	  
-      
-    // TEST CODE
-      
-    // STEP 6 PROMPT USER TO CHOOSE FROSTING
-	System.out.println("What type of FROSTING do you want? ");
-	System.out.println("Vanilla, Chocolate, Strawberry or Coco");
-	frostingType = keyboard.nextLine();
-
-   
-	 
-	 
-	 
-      
-    //TEST CODE
-      
-    // STEP 7 PROMPT USER TO CHOOSE FILLING
-	System.out.println("What type of FILLING do you want? ");
-	System.out.println("Mocha, Mint, Lemon, Caramel or Raspberry");
-	fillingType = keyboard.nextLine();
-
-
-	  
-	  
-      
-    // TEST CODE
-      
-    // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
-    	System.out.println("What type of TOPPINGS do you want? ");
-	System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
-	toppings = keyboard.nextLine();
-	
-
-
-	
-      
-      
-    // TEST CODE
-      
-    // STEP 9 DISPLAY ORDER CONFIRMATION
-	System.out.println();
-	System.out.println(firstName + " , your order is as follows: ");
-	System.out.println("_________________________________________");
-	System.out.println("Item Ordered: " + itemOrder);
-	System.out.println("Frosting: " + frostingType);
-	System.out.println("Filling: " + fillingType);
-	System.out.println("Toppings: " + toppings);
-	System.out.println("_________________________________________");
-	
-
-      
-	  
-	  
-	  
-    
-    // TEST CODE
-      
-    // STEP 10 DISPLAY COST AND SALES TAX
-   	System.out.printf("The cost of your order is: $%.2f\n", cost);
-	tax = cost * TAX_RATE;
-	System.out.printf("The tax is: $%.2f\n", tax);
-	System.out.printf("The total due is: $%.2f\n",(tax + cost));
-
-  
+        // DISPLAY COST AND SALES TAX
+        System.out.printf("The cost of your order is: $%.2f\n", totalCost+cost);
+        tax = (totalCost + cost) * TAX_RATE;
+        System.out.printf("The tax is: $%.2f\n", tax);
+        System.out.printf("The total due is: $%.2f\n",(tax + totalCost + cost));
     }   
 }
